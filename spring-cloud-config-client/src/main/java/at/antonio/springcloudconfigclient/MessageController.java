dc9e01af48ffa1191c0d9020dc92bf7d82126645
Antonio BITOUNIS <antonio.bitounis@rbinternational.com>
2023-06-09 15:50:52 +0200

    real-time refresh of properties

--- spring-cloud-config-client/src/main/java/at/antonio/springcloudconfigclient/MessageController.java
+++ spring-cloud-config-client/src/main/java/at/antonio/springcloudconfigclient/MessageController.java
@@ -4,11 +4,14 @@ import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope //Refresh props from the server without needing to restart anything (needs actuator dependency)
+//+needs a POST request to microservice endpoint localhost:8081/actuator/refresh
 public class MessageController {
 
     @Value("${ENV_VAR: No env var found :(}")
