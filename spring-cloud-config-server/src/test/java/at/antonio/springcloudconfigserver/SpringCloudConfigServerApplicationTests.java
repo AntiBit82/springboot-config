a0cdbe48f3f228d260bfec4d7a2c0ffc005d52f5
Antonio BITOUNIS <antonio.bitounis@rbinternational.com>
2023-06-09 13:13:16 +0200

    add Spring Cloud Config Server

---
+++ spring-cloud-config-server/src/test/java/at/antonio/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package at.antonio.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
